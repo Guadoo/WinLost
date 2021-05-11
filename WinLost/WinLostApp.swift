dbc9c8e3022f0e5280eb2a2997aad1b571765537
Liu Jingkai <rock_jk@msn.com>
2021-05-11 17:05:45 +0800

    Initial Commit

---
+++ WinLost/WinLostApp.swift
@@ -0,0 +1,17 @@
+//
+//  WinLostApp.swift
+//  WinLost
+//
+//  Created by Guadoo on 2021/5/11.
+//
+
+import SwiftUI
+
+@main
+struct WinLostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
